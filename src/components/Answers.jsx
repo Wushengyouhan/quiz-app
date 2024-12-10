a71b7ec81d5cfb1fa31953d70b1c74a34797d444
Wushengyouhan <840835067@qq.com>
2024-12-10 22:26:48 +0800

    201. Moving Logic To Components That Actually Need It ("Moving State Down")

---
+++ src/components/Answers.jsx
@@ -0,0 +1,50 @@
+import { useRef } from "react";
+
+export default function Answers({
+  answers,
+  selectedAnswer,
+  answerState,
+  onSelect,
+}) {
+  // 组件刷新不变化
+  const shuffledAnswers = useRef();
+
+  // 打乱顺序
+  if (!shuffledAnswers.current) {
+    shuffledAnswers.current = [...answers];
+    shuffledAnswers.current.sort(() => Math.random() - 0.5);
+  }
+
+  return (
+    <ul id="answers">
+      {shuffledAnswers.current.map((answer) => {
+        const isSelected = selectedAnswer === answer;
+        let cssClass = "";
+
+        // 已选择，未判断对错
+        if (answerState === "answered" && isSelected) {
+          cssClass = "selected";
+        }
+
+        if (
+          (answerState === "correct" || answerState === "wrong") &&
+          isSelected
+        ) {
+          cssClass = answerState;
+        }
+
+        return (
+          <li key={answer} className="answer">
+            <button
+              onClick={() => onSelect(answer)}
+              className={cssClass}
+              disabled={answerState !== ""}
+            >
+              {answer}
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,7 +1,8 @@
 import { useState, useCallback } from "react";
 import QUESTIONS from "../questions";
-import QuestionTimer from "./QuestionTimer";
 import quizCompletedImg from "../assets/quiz-complete.png";
+import Question from "./Question";
+
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
   const activeQuestionIndex = userAnswers.length;
@@ -29,29 +30,15 @@ export default function Quiz() {
       </div>
     );
   }
-  // 打乱当前的答案
-  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
-  shuffledAnswers.sort(() => Math.random() - 0.5);
 
   return (
     <div id="quiz">
-      <div id="question">
-        <QuestionTimer
-          key={activeQuestionIndex}
-          timeout={10000}
-          onTimeout={handleSkipAnswer}
-        />
-        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-        <ul id="answers">
-          {shuffledAnswers.map((answer) => (
-            <li key={answer} className="answer">
-              <button onClick={() => handleSelectAnswer(answer)}>
-                {answer}
-              </button>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <Question
+        key={activeQuestionIndex}
+        index={activeQuestionIndex}
+        onSelectAnswer={handleSelectAnswer}
+        onSkipAnswer={handleSkipAnswer}
+      />
     </div>
   );
 }
